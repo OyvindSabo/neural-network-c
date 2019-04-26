a8e9669f57dfb4205c3144db2eaa25b53cd6bdc9
oyvindsabo <oyvind.sabo@gmail.com>
2019-04-26 12:46:28 +0200

    Create feedForward method with a few problems

--- src/networkUtils.c
+++ src/networkUtils.c
@@ -1,3 +1,48 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "network.c"
+
+struct InputValues {
+  double values[100];
+  int length;
+};
+
 double sigmoid(double x) {
   return 1 / (1 + exp(-x));
 }
+
+void feedForward(struct Network *network, struct InputValues inputValues, bool useNewWeights) {
+
+  if (inputValues.length != (*network).inputLength) {
+    printf("ERROR: Length of input data is not equal to length of input layer\n");
+  }
+
+  // Set input layer values
+  for (int i = 0; i < (*network).inputLength; i++) {
+    (*network).inputLayer[i].value = inputValues.values[i];
+  }
+
+  // Calculate hidden layer values
+  for (int i = 0; i < (*network).hiddenLength; i++) {
+    (*network).hiddenLayer[i].value = 0;
+    for (int j = 0; j < (*network).hiddenLayer[i].inECount; j++) {
+      float currentWeight = (*network).hiddenLayer[i].inE[j].currentWeight;
+      float newWeight = (*network).hiddenLayer[i].inE[j].newWeight;
+      float value = (*network).hiddenLayer[i].inE[j].value;
+      (*network).hiddenLayer[i].value += (useNewWeights ? newWeight : currentWeight) * value;
+    }
+    (*network).hiddenLayer[i].value = sigmoid((*network).hiddenLayer[i].value);
+  }
+
+  // Calculate output layer values
+  for (int i = 0; i < (*network).outputLength; i++) {
+    (*network).outputLayer[i].value = 0;
+    for (int j = 0; j < (*network).outputLayer[i].inECount; j++) {
+      float currentWeight = (*network).outputLayer[i].inE[j].currentWeight;
+      float newWeight = (*network).outputLayer[i].inE[j].newWeight;
+      float value = (*network).outputLayer[i].inE[j].value;
+      (*network).outputLayer[i].value += (useNewWeights ? newWeight : currentWeight) * value;
+    }
+    (*network).outputLayer[i].value = sigmoid((*network).outputLayer[i].value);
+  }
+};
